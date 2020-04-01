078fe980f7041ec87f31feafe2f07b5f1adc6a8a
alu0101100654 <alu0101100654@ull.edu.es>
2020-04-01 16:51:01 +0100

    Pruebas en Jest realizadas

---
+++ @ULL-ESIT-PL-1920/p10-t2-lexgen-code-alu0101100654.js
@@ -0,0 +1,81 @@
+/**
+ * @desc Función que construye y devuelve una función que permite
+ * realizar un análisis léxico simple de un lenguaje especificado.
+ * La especificación del lenguaje se realiza a través de un array de
+ * pares  NombreDelToken - /regExpDelToken/.
+ * @param {Aray} tokens Array de pares NombreDelToken - /regExpDelToken/.
+ * @returns {Function} Función que puede realizar el análisis
+ * léxico del lenguaje especificado.
+ */
+function buildLexer(tokens) {
+  return function(stringChain) {
+    const tokenNames = tokens.map(token => token[0]);
+    if (tokenNames.indexOf('ERROR') !== -1) {
+      throw new Error('Error. No se permite un token de tipo ERROR');
+    }
+    const tokenRegs = tokens.map(token => token[1]);
+
+    /**
+     * @desc Función que une un conjunto de expresiones regulares a través
+     * de conjunciones.
+     * @param {Array} regexps
+     * @return {Regexp} Expresión regular formada por la aplicación de
+     * conjunciones a las expresiones regulares que conforman el array "regexps". 
+     */
+    function buildOrRegexp(regexps) {
+      const sources = regexps.map(regExp => regExp.source);
+      const union = sources.join('|');
+      return new RegExp(union, 'y');
+    }
+
+    /**
+     * @desc Función que permite la creación de un token en forma de objeto JS.
+     * @param {String} tokenType Tipo al que se asocia el token.
+     * @param {String} tokenValue Valor que toma el token.
+     * @return {Object} Objecto que representa un token. Su estructura es la
+     * siguiente: {type: tokenType, value: tokenValue}.
+     */
+    function makeTokenObject(tokenType, tokenValue) {
+      return {
+        type: tokenType,
+        value: tokenValue,
+      };
+    }
+
+    /**
+     * @desc Función que comprueba si el tipo del token es operable. Esto es,
+     * si no representa a algún elemento del lenguaje sin valor asociado.
+     * Estos elementos son, en esencia, espacios y comentarios.
+     * @param {String} tokenType Tipo del token a comprobar.
+     * @return {Boolean} True si el token es operable. False en caso contrario.
+     */
+    function isTypeOperable(tokenType) {
+      const tokenTypeUpper = tokenType.toUpperCase();
+      if (tokenTypeUpper === 'WHITE' || tokenTypeUpper === 'COMMENT' ||
+          tokenType === 'SINGLECOMMENT' || tokenType === 'SPACE') {
+          return false;
+      } else {
+        return true;
+      }
+    }
+
+    const orRegexp = buildOrRegexp(tokenRegs);
+    const getToken = (m) => tokenNames.find(tn => typeof m[tn] !== 'undefined');
+    let match;
+    const result = [];
+    let lastIndex;
+    while (match = orRegexp.exec(stringChain)) {
+      let tokenType = getToken(match.groups);
+      lastIndex = orRegexp.lastIndex;
+      if (isTypeOperable(tokenType)) {
+        result.push(makeTokenObject(tokenType, match.groups[tokenType]));
+      }
+    }
+    if (lastIndex !== stringChain.length) {
+      result.push(makeTokenObject('ERROR', stringChain.substr(lastIndex)));
+    }
+    return result;
+  };
+}
+
+module.exports.buildLexer = buildLexer;
---
+++ test.js
@@ -0,0 +1,113 @@
+const debug = process.env["DEBUG"];
+const { inspect } = require('util');
+const ins = (x) => { if (debug) console.log(inspect(x, {depth: null})) };
+
+const buildLexer = require('./@ULL-ESIT-PL-1920/p10-t2-lexgen-code-alu0101100654.js');
+
+const SPACE = /(?<SPACE>\s+|\/\/.*)/;
+const RESERVEDWORD = /(?<RESERVEDWORD>\b(const|let)\b)/;
+const ID = /(?<ID>\b([a-z_]\w*))\b/;
+const STRING = /(?<STRING>"([^\\"]|\\.")*")/;
+const OP = /(?<OP>[+*\/=-])/;
+
+const myTokens = [
+  ['SPACE', SPACE], ['RESERVEDWORD', RESERVEDWORD], ['ID', ID],
+  ['STRING', STRING], ['OP', OP]
+];
+
+let str, lexer, r;
+lexer = buildLexer.buildLexer(myTokens);
+
+str = 'const varName = "value"';
+ins(str);
+r = lexer(str);
+ins(r);
+let expected = [
+  { type: 'RESERVEDWORD', value: 'const' },
+  { type: 'ID', value: 'varName' },
+  { type: 'OP', value: '=' },
+  { type: 'STRING', value: '"value"' }
+];
+
+test(str, () => {
+  expect(r).toEqual(expected);
+});
+
+str = 'let x = a + \nb';
+ins(str);
+r = lexer(str);
+expected = [
+  { type: 'RESERVEDWORD', value: 'let' },
+  { type: 'ID', value: 'x' },
+  { type: 'OP', value: '=' },
+  { type: 'ID', value: 'a' },
+  { type: 'OP', value: '+' },
+  { type: 'ID', value: 'b' }
+];
+ins(r);
+test(str, () => {
+  expect(r).toEqual(expected);
+});
+
+str = ' // Entrada con errores\nlet x = 42*c';
+ins(str);
+r = lexer(str);
+ins(r);
+expected = [
+  { type: 'RESERVEDWORD', value: 'let' },
+  { type: 'ID', value: 'x' },
+  { type: 'OP', value: '=' },
+  { type: 'ERROR', value: '42*c' }
+];
+
+test(str, () => {
+  expect(r).toEqual(expected);
+});
+
+// Nuevas adiciones
+str = ' // /*dsdadas*/ //';
+ins(str);
+r = lexer(str);
+expected = [];
+
+test(str, () => {
+  expect(r).toEqual(expected);
+});
+
+const tokensWithError = [
+  ['SPACE', SPACE], ['RESERVEDWORD', RESERVEDWORD], ['ID', ID],
+['STRING', STRING], ['OP', OP], ['ERROR', /.*/]
+];
+
+const newLexer = buildLexer.buildLexer(tokensWithError);
+test(str, () => {
+  expect(() => newLexer(str)).toThrow(Error);
+});
+
+str = 'Esto solo debería dar un error Ç+3\nY no debería dar dos Ç+3';
+ins(str);
+r = lexer(str);
+expected = [
+  {
+    type: 'ERROR',
+    value: 'Esto solo debería dar un error Ç+3\nY no debería dar dos Ç+3'
+  }
+];
+
+test(str, () => {
+  expect(r).toEqual(expected);
+});
+
+str = 'Esta cadena de texto debería ser inválida\n';
+ins(str);
+r = lexer(str);
+expected = [
+  {
+    type: 'ERROR',
+    value: 'Esta cadena de texto debería ser inválida\n',
+  }
+];
+
+test(str, () => {
+  expect(r).toEqual(expected);
+});
